c59f949eea9a3857bf4df6ce5a5d6d811f0c0460
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-11-17 16:09:37 +0300

    added a get char count dictionary. Printed each character and its count without duplicates. refactored the print into its own function to clean up main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_character_count
 
 def get_book_text(path_to_file):
     file_contents = ""
@@ -6,9 +7,16 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
+def print_character_counts(characters):
+    for c in characters:
+        print(f"'{c}': {characters[c]}")    
+
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    num_words = get_num_words(text)
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = get_num_words(book_text)
+    total_characters = get_character_count(book_text) 
     print(f"Found {num_words} total words")
+    print_character_counts(total_characters)
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+# returns a dictionary
+# convert to lower case .lower() so no duplicates
+# use dictionary String -> int 
+# example: {'p': 6121, 'r': 20818, 'o': 25225, ...
+def get_character_count(text):
+    characters = {}
+    for letter in text.lower():
+        characters[letter] = characters.get(letter, 0) + 1  
+    return characters
