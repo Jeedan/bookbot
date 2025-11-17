f93d50de73d2e3fff7d8a55675e5ca6d1b997470
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-11-17 15:43:33 +0300

    refactored count_words into get_num_words for clarity and moved it into a stats.py file

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     file_contents = ""
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-def count_words(text):
-    words = text.split()
-    return len(words)
-
 def main():
     text = get_book_text("books/frankenstein.txt")
-    num_words = count_words(text)
+    num_words = get_num_words(text)
     print(f"Found {num_words} total words")
 
 main()
