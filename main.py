0f66fc8d9bc78833da034d24e0fea339871a8f71
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-11-17 21:39:15 +0300

    sorted list of dictionaries, pretty display of results. Passed Boots

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from stats import get_num_words
-from stats import get_character_count
+from stats import get_num_words, get_character_count, sort_characters
 
 def get_book_text(path_to_file):
     file_contents = ""
@@ -8,15 +7,27 @@ def get_book_text(path_to_file):
     return file_contents
 
 def print_character_counts(characters):
-    for c in characters:
-        print(f"'{c}': {characters[c]}")    
+    for chars in characters:
+        print(f"'{chars["char"]}: {chars["count"]}'")  
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    #store the name of the book, maybe we want to get user input later
+    book_name = "frankenstein.txt"
+    book_text = get_book_text(f"books/{book_name}")
     num_words = get_num_words(book_text)
-    total_characters = get_character_count(book_text) 
-    print(f"Found {num_words} total words")
-    print_character_counts(total_characters)
 
+    total_characters = get_character_count(book_text) 
+    # we sort the characters from highest to lowest count
+    sorted_characters = sort_characters(total_characters)
+
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at books/{book_name}...")
+    print(f"----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print(f"----------- Character Count ----------")
+    #print our sorted_character list of dictionaries
+    print_character_counts(sorted_characters)
+    
+    print(f"============ END ============")
 
 main()
--- stats.py
+++ stats.py
@@ -9,5 +9,20 @@ def get_num_words(text):
 def get_character_count(text):
     characters = {}
     for letter in text.lower():
-        characters[letter] = characters.get(letter, 0) + 1  
+        characters[letter] = characters.get(letter, 0) + 1 
     return characters
+
+def sort_on(items):
+    return items["count"]
+
+# I consulted Boots and Chat GPT to get this to work
+# i guess I need more training on Dictionaries
+def sort_characters(characters):
+    char_list = []
+    #char_list = [{"char":c,"count":n} for c,n in characters.items()]
+
+    for c, n in characters.items():
+        char_list.append({"char" : c, "count": n})
+
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
